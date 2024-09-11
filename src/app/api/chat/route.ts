8f4d68b22d4ab93ab20e953e1e2711100289395a
Terry Tang <59753147+sayaneko514@users.noreply.github.com>
2024-09-11 09:28:35 -0700

    feat(api): added gpt tools in the back-end

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,30 +1,29 @@
 import { openai } from '@ai-sdk/openai';
 import { convertToCoreMessages, streamText } from 'ai';
 import { NextRequest, NextResponse } from 'next/server';
+import { bookingSchema, handleBooking } from '@/utils/bookingSchema';
+import { getPSTDate } from '@/utils/getDate';
 
 export const maxDuration = 30;
-
-export const runtime = 'edge'
+export const runtime = 'edge';
 
 export async function POST(req: NextRequest) {
-
-  try{
+  try {
     if (!process.env.OPENAI_API_KEY) {
-      return new NextResponse('Missing API Key.', {status: 400 })
+      return new NextResponse('Missing API Key.', { status: 400 });
     }
 
     const { messages } = await req.json();
-
-    const { protocol } = new URL(req.url); 
+    const { protocol } = new URL(req.url);
     const host = req.headers.get('host');
 
-    const fetchServiceDataResponse = await fetch(`${protocol}//${host}/api/fetch-service-data`);
+    const fetchServiceDataResponse = await fetch(`${protocol}//${host}/api/fetchServiceData`);
     const serviceData = await fetchServiceDataResponse.json();
     const servicesInfo = serviceData.map((service: any) => {
       return `**${service.service_name}**: $${service.price}`;
     }).join('\n');
 
-    const fetchOpsDataResponse = await fetch(`${protocol}//${host}/api/fetch-ops-data`);
+    const fetchOpsDataResponse = await fetch(`${protocol}//${host}/api/fetchOpsData`);
     const operationData = await fetchOpsDataResponse.json();
     const operationInfo = `
       Store Address: ${operationData.find((item: any) => item.address)?.address || "Not Available"}
@@ -32,26 +31,34 @@ export async function POST(req: NextRequest) {
       Payment Methods: ${(operationData.find((item: any) => item.payment_method)?.payment_method || []).join(', ')}
       After Hour Service Available: ${operationData.find((item: any) => item.after_hour)?.after_hour ? 'Yes' : 'No'}
     `;
+    const currentPSTDate = getPSTDate();
 
     const result = await streamText({
       model: openai('gpt-4o-mini'),
-      system: 
-        `You are Naomi's virtual receptionist, responsible for managing customer inquiries and booking appointments. Your main tasks include:
-        1. Provide accurate and helpful responses based on the information available to you.
-        2. Use ${servicesInfo} to answer inquery regarding to services and prices.
-        3. Use ${operationInfo} to answer inquery regarding store infomation and operating hours.
-        4. Assist customers in booking appointments, allowing bookings up to two weeks in advance. If a preferred time slot is unavailable, suggest alternative dates, times, or after-hour options.
-        5. For rescheduling, canceling, or after-hour bookings, offer the customer a contact method to reach Naomi directly for further assistance.
-        Your goal is to ensure a smooth and pleasant experience for customers while maintaining Naomi's schedule efficiently.`,
+      system: `
+        You are a virtual receptionist for (Nu HairLab), operated by junior hairstylist Naomi Ni. 
+        Collect customer's name, phone number, and date for booking and rescheduling.
+        Collect customer's phone number for cancellation.
+        Confirm the booking information before proceeding with the booking.
+        The current date and time is ${currentPSTDate} (PST).
+        After booking ask if additional help is needed, do not repeat booking information after confirmation.
+        Use the following service info for inquiries: ${servicesInfo}.
+        Use the store info for inquiries about operations: ${operationInfo}.
+        Ensure a smooth and helpful experience for all inquiries.
+      `,
       messages: convertToCoreMessages(messages),
+      tools: {
+        bookAppointment: {
+          description: "Get booking information from the customer and handle appointment logic.",
+          parameters: bookingSchema,
+          execute: handleBooking,
+        },
+      },
+      experimental_toolCallStreaming: true,
     });
-    
+
     return result.toDataStreamResponse();
   } catch (error: any) {
-    return new NextResponse(error.message || 'Something went wrong, try refresh the page.', {status: 500})
+    return new NextResponse(error.message || 'Something went wrong, try refreshing the page.', { status: 500 });
   }
-}
-
-function useState<T>(arg0: never[]): [any, any] {
-  throw new Error('Function not implemented.');
 }
